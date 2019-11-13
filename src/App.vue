076f36532ebcbaab00e14a03f42d4d6ce2fa6b00
Bachir Ndiaye <ndiayeb44@gmail.com>
2019-11-13 13:41:32 +0000

    app root color changed

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #707070;
   overflow-x: hidden;
 }
 </style>
