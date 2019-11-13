13a90b5e0e75e9761ffa77e939baf4f74c93f729
Bachir Ndiaye <ndiayeb44@gmail.com>
2019-11-13 15:25:21 +0000

    Font awesome minus (-) and plus (+) added

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFontAwesome, faSnapchat, faFacebookSquare, faTwitter, faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-library.add(faSnapchat, faFacebookSquare, faTwitter, faWhatsapp, faInstagram, faFontAwesome);
+library.add(faSnapchat, faFacebookSquare, faTwitter, faWhatsapp, faInstagram, faFontAwesome, faPlus, faMinus);
 
 Vue.use(BootstrapVue);
 
