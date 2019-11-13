fbb15bdcaf3726a4d6f1516a79996d5da17d78cf
Bachir Ndiaye <ndiayeb44@gmail.com>
2019-11-13 17:51:55 +0000

    'About' page is done √

--- src/components/SimpleVuejsShopAbout.vue
+++ src/components/SimpleVuejsShopAbout.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="SimpleVuejsShopAbout">
-    <h1>ABOUT !</h1>
+    <h1 class="text-capitalize title">{{ pageData.title }}</h1>
+    <div class="offset-3 col-6 content">
+      {{ pageData.content }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'SimpleVuejsShopAbout'
+  name: 'SimpleVuejsShopAbout',
+  data() {
+    return {
+      pageData: null
+    }
+  },
+  created() {
+    axios.get('http://localhost:5500/data.json')
+      .then(response => this.pageData = response.data.about);
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.SimpleVuejsShopAbout {
+  margin: 40px 0;
+  font-family: snell
+}
 
+.content {
+  color: #000;
+  font-size: 1.5em;
+}
 </style>
